bbab30777a4f963f90a3d5e375566261ad292632
Nandhitha <46625586+nandhitha1999@users.noreply.github.com>
2020-05-30 22:55:09 +0530

    code

---
+++ week2/ex1/warmUpExercise.m
@@ -0,0 +1,14 @@
+function A = warmUpExercise()
+%WARMUPEXERCISE Example function in octave
+%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = [];
+A=eye(5);
+
+
+
+
+% ===========================================
+
+
+end
